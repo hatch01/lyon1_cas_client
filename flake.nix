59fd87f22228285ee8344edeb1a41886f45436d3
eymeric <eymericdechelette@gmail.com>
2024-11-14 16:55:13 +0100

    where clippy ?

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
             buildInputs = [
               (rust-bin.stable.latest.default.override {
-                extensions = ["rust-analyzer" "rust-src" "rustfmt"];
+                extensions = ["rust-analyzer" "rust-src" "rustfmt" "clippy"];
               })
             ];
 
